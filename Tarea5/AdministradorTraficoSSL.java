7e10799bb8e0dbe66798b44968e019e0ada17232
Ramses Llamosas <gonzalezllamosas07@gmail.com>
2025-04-24 16:14:54 -0600

    Se añade carpeta de la Tarea 5

--- Tarea5/AdministradorTraficoSSL.java
+++ Tarea5/AdministradorTraficoSSL.java
@@ -1,6 +1,6 @@
 /*
-  AdministradorTrafico.java
-  Generado por ChatGPT a partir del programa Proxy.java
+  AdministradorTraficoSSL.java
+  Generado por ChatGPT modificado por LlamosasNG
 */
 
 import java.io.InputStream;
@@ -10,27 +10,22 @@ import java.net.Socket;
 import java.net.ServerSocket;
 import javax.net.ssl.SSLServerSocketFactory;
 
-class AdministradorTraficoSSL
-{
+class AdministradorTraficoSSL {
   static String host_remoto_1;
   static int puerto_remoto_1;
   static String host_remoto_2;
   static int puerto_remoto_2;
   static int puerto_local;
 
-  static class Worker_1 extends Thread
-  {
+  static class Worker_1 extends Thread {
     Socket cliente_1, servidor_1, servidor_2;
-    
-    Worker_1(Socket cliente_1)
-    {
+
+    Worker_1(Socket cliente_1) {
       this.cliente_1 = cliente_1;
     }
 
-    public void run()
-    {
-      try
-      {
+    public void run() {
+      try {
         // Se conecta al primer host remoto
         servidor_1 = new Socket(host_remoto_1, puerto_remoto_1);
 
@@ -46,85 +41,70 @@ class AdministradorTraficoSSL
         byte[] buffer = new byte[1024];
         int n;
 
-        while((n = entrada_1.read(buffer)) != -1)
-        {
+        while ((n = entrada_1.read(buffer)) != -1) {
           // Reenvía los datos del cliente al primer servidor remoto
           salida_1.write(buffer, 0, n);
           salida_1.flush();
 
-          // Reenvía los datos del cliente al segundo servidor remoto (sin esperar la respuesta)
+          // Reenvía los datos del cliente al segundo servidor remoto (sin esperar la
+          // respuesta)
           salida_2.write(buffer, 0, n);
           salida_2.flush();
         }
-      }
-      catch (IOException e)
-      {
-      }
-      finally
-      {
-        try
-        {
-          if (cliente_1 != null) cliente_1.close();
-          if (servidor_1 != null) servidor_1.close();
-          if (servidor_2 != null) servidor_2.close();
-        }
-        catch (IOException e2)
-        {
+      } catch (IOException e) {
+      } finally {
+        try {
+          if (cliente_1 != null)
+            cliente_1.close();
+          if (servidor_1 != null)
+            servidor_1.close();
+          if (servidor_2 != null)
+            servidor_2.close();
+        } catch (IOException e2) {
           e2.printStackTrace();
         }
       }
     }
   }
 
-  static class Worker_2 extends Thread
-  {
+  static class Worker_2 extends Thread {
     Socket cliente_1, servidor_1;
 
-    Worker_2(Socket cliente_1, Socket servidor_1)
-    {
+    Worker_2(Socket cliente_1, Socket servidor_1) {
       this.cliente_1 = cliente_1;
       this.servidor_1 = servidor_1;
     }
 
-    public void run()
-    {
-      try
-      {
+    public void run() {
+      try {
         InputStream entrada_1 = servidor_1.getInputStream();
         OutputStream salida_1 = cliente_1.getOutputStream();
         byte[] buffer = new byte[4096];
         int n;
 
-        while((n = entrada_1.read(buffer)) != -1)
-        {
+        while ((n = entrada_1.read(buffer)) != -1) {
           // Solo reenvía la respuesta del servidor 1 al cliente
           salida_1.write(buffer, 0, n);
           salida_1.flush();
         }
-      }
-      catch (IOException e)
-      {
-      }
-      finally
-      {
-        try
-        {
-          if (cliente_1 != null) cliente_1.close();
-          if (servidor_1 != null) servidor_1.close();
-        }
-        catch (IOException e2)
-        {
+      } catch (IOException e) {
+      } finally {
+        try {
+          if (cliente_1 != null)
+            cliente_1.close();
+          if (servidor_1 != null)
+            servidor_1.close();
+        } catch (IOException e2) {
           e2.printStackTrace();
         }
       }
     }
   }
 
-  public static void main(String[] args) throws Exception
-  {
-    if (args.length != 5)
-    {
-      System.err.println("Uso:\njava AdministradorTrafico <host-remoto-1> <puerto-remoto-1> <host-remoto-2> <puerto-remoto-2> <puerto-local>");
+  public static void main(String[] args) throws Exception {
+    if (args.length != 5) {
+      System.err.println(
+          "Uso:\njava AdministradorTrafico <host-remoto-1> <puerto-remoto-1> <host-remoto-2> <puerto-remoto-2> <puerto-local>");
       System.exit(1);
     }
 
@@ -134,7 +114,8 @@ class AdministradorTraficoSSL
     puerto_remoto_2 = Integer.parseInt(args[3]);
     puerto_local = Integer.parseInt(args[4]);
 
-    System.out.println("Conectando a " + host_remoto_1 + ":" + puerto_remoto_1 + " y " + host_remoto_2 + ":" + puerto_remoto_2 + " en puerto local " + puerto_local);
+    System.out.println("Conectando a " + host_remoto_1 + ":" + puerto_remoto_1 + " y " + host_remoto_2 + ":"
+        + puerto_remoto_2 + " en puerto local " + puerto_local);
 
     System.setProperty("javax.net.ssl.keyStore", "keystore_servidor.jks");
     System.setProperty("javax.net.ssl.keyStorePassword", "1234567");
@@ -142,8 +123,7 @@ class AdministradorTraficoSSL
 
     ServerSocket ss = socket_factory.createServerSocket(puerto_local);
 
-    for(;;)
-    {
+    for (;;) {
       // Espera una conexión del cliente
       Socket cliente_1 = ss.accept();
 
